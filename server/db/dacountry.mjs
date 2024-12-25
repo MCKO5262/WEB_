5e621dfc28860981432c665104369a34c1edb121
My name <154625410+SoDKo0331@users.noreply.github.com>
2024-12-25 15:11:54 +0800

    mes

--- server/db/dacountry.mjs
+++
@@ -1,9 +0,0 @@
-export default class DaCountry {
-    constructor(poolObj) {
-        this.db = poolObj;
-        this.Query = "query;"
-    }
-    async listCountries() { 
-            return await this.db.query(this.Query);
-    }
-}
