9be2a0d0a578a2eaec2ef1a5583a15ac209cf1bb
My name <154625410+SoDKo0331@users.noreply.github.com>
2025-01-04 02:27:35 +0800

    meas

--- js/Apiartist.js
+++ js/Apiartist.js
@@ -1,6 +1,6 @@
 class ApiService {
     constructor() {
-      this.apiUrl = 'https://api.jsonbin.io/v3/qs/6777a764ad19ca34f8e4f702'; // Unified API endpoint
+      this.apiUrl = 'https://api.jsonbin.io/v3/qs/6778281eacd3cb34a8c3ab2a'; // Unified API endpoint
     }
   
     async fetchArtists() {
